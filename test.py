fe76bcfb4ea405efe1ae2f02a7695269e4ba589e
Hao323 <haojiachengzs@outlook.com>
2025-01-24 18:17:53 +0000

    Update test.py

--- test.py
+++ test.py
@@ -40,3 +40,60 @@ def calculate_gcd_with_extension(a, b):
         print("The numbers are not relatively prime.")
 
    return gcd
+
+
+
+#Simplify a fraction using the GCD.
+def simplify_fraction(x , y):
+   
+    # Divide both numerator and denominator by the GCD
+    gcd = c_gcd(x, y)
+    up = x // gcd
+    down = y //gcd
+    
+    print(f"Simplified fraction: {up} / {down}")
+    return up , down
+
+
+#Extension allows users to enter numbers
+def user():
+    
+    #Provide options to let users choose the desired action
+    while True:
+        print("\nChoose an option:")
+        print("1. Calculate GCD")
+        print("2. Simplify a fraction")
+        
+        choice = input("Enter your choice (1-2): ")
+        
+        if choice == "1" :
+        
+            # Input two positive integers from the user
+            try:
+                num1 = int(input("Please enter a number"))
+                num2 = int(input("please enter a number"))
+                
+                # Calculate GCD
+                calculate_gcd_with_extension(num1, num2)
+                
+                
+            except ValueError:
+                print("Error: Please enter valid integers")
+        
+        elif choice == "2" :
+            
+            #Enter the numerator and denominator
+            x = int(input("Enter the x : "))
+            y = int(input("Enter the y : "))
+            
+            #Eliminate the invalid case where the denominator is equal to 0
+            if y == 0:
+                print("Error: y cannot be zero.")
+                
+            else:
+                simplify_fraction(x , y )
+            
+                    
+# Run the program
+if __name__ == "__user__":
+    user()
